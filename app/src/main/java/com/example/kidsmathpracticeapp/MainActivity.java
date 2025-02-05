21cf15e3fe7bb1f5f869d87557c37a16ec3bf304
mjilani98 <mjilani@emich.edu>
2025-02-05 15:21:28 -0500

    added a working next buttn

--- app/src/main/java/com/example/kidsmathpracticeapp/MainActivity.java
+++ app/src/main/java/com/example/kidsmathpracticeapp/MainActivity.java
@@ -25,13 +25,6 @@ public class MainActivity extends AppCompatActivity {
          generator = new Generator();
 
         initialize(generator);
-
-
-
-
-
-
-
     }
 
     public void initialize(Generator generator)
@@ -122,6 +115,59 @@ public class MainActivity extends AppCompatActivity {
         //next button , enabled
         Button nextBtn = findViewById(R.id.NextButton);
         nextBtn.setEnabled(true);
+    }
+
+    public void next(View v)
+    {
+        //generate
+        generator.generate();
+
+        //get operator , two numbers, correct, total
+        //display two numbers, operator
+
+        //display number 1
+        TextView output1 = findViewById(R.id.Num1);
+        String strNum1 = generator.getNum1()+"";
+        output1.setText(strNum1);
+
+        //display number 2
+        TextView output2 = findViewById(R.id.Num2);
+        String strNum2 = generator.getNum2()+"";
+        output2.setText(strNum2);
+
+        //set the operator text view
+        TextView operatorView = findViewById(R.id.Operator);
+        String operator = "";
+        if(generator.getOperator() == 1)
+            operator = "+";
+        if(generator.getOperator() == 2)
+            operator = "-";
+        if(generator.getOperator() == 3)
+            operator = "*";
+        if(generator.getOperator() == 4)
+            operator = "/";
+        operatorView.setText(operator);
+
+        //display correct, total
+        //get the total answers and correct answers
+        TextView scoreOutPut = findViewById(R.id.Scores);
+        String strCorrectAnswer = generator.getTotalCorrectAnswers()+"";
+        String strTotalQuestions = generator.getTotalQuestions()+"";
+        scoreOutPut.setText(strCorrectAnswer+"/"+strTotalQuestions);
+
+        //enable submit button, disable submit button
+
+        //submit button , disabled
+        Button submitBtn = findViewById(R.id.SubmitButton);
+        submitBtn.setEnabled(true);
+
+        //next button , enabled
+        Button nextBtn = findViewById(R.id.NextButton);
+        nextBtn.setEnabled(false);
+
+        //clearing the input from the previous question
+        EditText input = findViewById(R.id.AnswerInputBox);
+        input.setText("");
     }
 
 

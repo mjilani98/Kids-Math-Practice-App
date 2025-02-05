ffd0327e7f20e1efba1a055079ad01b35e3d66eb
mjilani98 <mjilani@emich.edu>
2025-02-05 14:28:31 -0500

    Added a working submit button

--- app/src/main/java/com/example/kidsmathpracticeapp/MainActivity.java
+++ app/src/main/java/com/example/kidsmathpracticeapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.kidsmathpracticeapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Generator generator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //create a generator object
-        Generator generator = new Generator();
+         generator = new Generator();
 
         initialize(generator);
 
@@ -33,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void initialize(Generator generator)
     {
-
+        // - generate
         generator.generate();
 
         //display number 1
@@ -45,8 +48,6 @@ public class MainActivity extends AppCompatActivity {
         TextView output2 = findViewById(R.id.Num2);
         String strNum2 = generator.getNum2()+"";
         output2.setText(strNum2);
-
-
 
         //set the operator text view
         TextView operatorView = findViewById(R.id.Operator);
@@ -61,19 +62,66 @@ public class MainActivity extends AppCompatActivity {
             operator = "/";
         operatorView.setText(operator);
 
-
         //get the total answers and correct answers
         TextView scoreOutPut = findViewById(R.id.Scores);
+        scoreOutPut.setText("0/0");
 
-        String strCorrectAnswer = generator.getTotalCorrectAnswers()+"";
-        String strTotalQuestions = generator.getTotalQuestions()+"";
+        //- enable submit button, disable next button
 
-        scoreOutPut.setText(strCorrectAnswer + "/" + strTotalQuestions);
+        //submit button
+        Button submitBtn = findViewById(R.id.SubmitButton);
+        submitBtn.setEnabled(true);
+
+        //next button
+        Button nextBtn = findViewById(R.id.NextButton);
+        nextBtn.setEnabled(false);
     }
 
     public void submit(View v)
     {
-        
+        //read a users answer
+        EditText userInput = findViewById(R.id.AnswerInputBox);
+        String strUserInput = userInput.getText().toString();
+        int userAnswer=0;
+        try {
+             userAnswer = Integer.parseInt(strUserInput);
+        }
+        catch (NumberFormatException e) // if the user enters an empty answer
+        {
+            generator.incrementTotalQuestions();
+        }
+
+        // if the user did not enter an empty answer , check if the answer is right
+        if(!strUserInput.isEmpty())
+        {
+            //get answer
+            int answer = generator.getCorrectAnswer();
+
+            //if users answer equal answer , increase correct
+            if(userAnswer == answer)
+            {
+                generator.incrementCorrectAnswers();
+                generator.incrementTotalQuestions();
+            }
+            else
+                generator.incrementTotalQuestions();
+        }
+
+        //getting the number of correct answers and number of questions
+        String strCorrectAnswer = generator.getTotalCorrectAnswers()+"";
+        String strTotalQuestions = generator.getTotalQuestions()+"";
+
+        //getting hold of the text view and display the score
+        TextView scoreOutPut = findViewById(R.id.Scores);
+        scoreOutPut.setText(strCorrectAnswer+"/"+strTotalQuestions);
+
+        //submit button , disabled
+        Button submitBtn = findViewById(R.id.SubmitButton);
+        submitBtn.setEnabled(false);
+
+        //next button , enabled
+        Button nextBtn = findViewById(R.id.NextButton);
+        nextBtn.setEnabled(true);
     }
 
 

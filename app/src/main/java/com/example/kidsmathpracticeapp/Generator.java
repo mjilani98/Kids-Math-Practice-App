67d64c07345c77d145a43526923d0232ba147e20
mjilani98 <mjilani@emich.edu>
2025-02-05 12:36:03 -0500

    Modified some issues in the Generator class Added initialze method in the main (controller)

--- app/src/main/java/com/example/kidsmathpracticeapp/Generator.java
+++ app/src/main/java/com/example/kidsmathpracticeapp/Generator.java
@@ -15,52 +15,42 @@ public class Generator {
     //The type of operation, randomly will be generated
     private int operator;
     //The total number of questions been answered
-    private int totalQuestions;
+    private int totalQuestions  = 0;
     //The total number of the right answers
     private int totalCorrectAnswers = 0 ;
 
 
     //create a Random class object
     private Random random = new Random();
-    //public constructor
-    public Generator()
+
+    public void generate()
     {
         //the operator will range between 1 and 4 , both inclusive
         operator = random.nextInt(4) +1 ;
-        totalQuestions +=1;
+
+        if(operator == 1)
+        {
+            num1 = random.nextInt(999)+1;
+            num2 = random.nextInt(999)+1;
+        }
+        else if(operator == 2)
+        {
+            num1 = random.nextInt(999)+1;
+            num2 = random.nextInt(999)+1;
+        }
+        else if(operator == 3)
+        {
+            num1 = random.nextInt(99)+1;
+            num2 = random.nextInt(99)+1;
+        }
+        else if(operator == 3)
+        {
+            num1 = random.nextInt(999)+1;
+            num2 = random.nextInt(99)+1;
+        }
 
     }
-    // if the operation is less or equal to 2 it is Addition or subtraction
-    // if the operation is more than 2 is is Multiplication or Division
 
-    //get number 1
-    public int getNum1()
-    {
-        //if the operation is Addition or subtraction , number range between 1 - 999
-        if(operator <= 2)
-            num1 = random.nextInt(999) + 1;
-        else
-            //if the operation is Multiplication or Division , number range between 1 - 99
-            num1 = random.nextInt(99) + 1;
-
-        return num1;
-    }
-
-    //get number 2
-    public int getNum2()
-    {
-        //if the operation is Addition or subtraction , number range between 1 - 999
-        //if the operation is Division , the number 2 has to be between 1 - 99
-        if(operator <= 3)
-            num2 = random.nextInt(999) + 1;
-        else
-            //if the operation is Multiplication or Division , number range between 1 - 99
-            num2 = random.nextInt(99) + 1;
-
-        return num2;
-    }
-
-    //get the correct answer
     public int getCorrectAnswer()
     {
         //if Addition
@@ -77,6 +67,23 @@ public class Generator {
         return correctAnswer;
     }
 
+
+
+    //get number 1
+    public int getNum1()
+    {
+        return num1;
+    }
+
+    //get number 2
+    public int getNum2()
+    {
+        return num2;
+    }
+
+    //get the correct answer
+
+
     //get total questions
     public int getTotalQuestions()
     {
@@ -84,14 +91,24 @@ public class Generator {
     }
 
     //get the total of correct answers
-    public int getTotCorrectAnswers()
+    public int getTotalCorrectAnswers()
     {
         return totalCorrectAnswers;
     }
 
-    public void incrementCorrectAnswes()
+    public void incrementCorrectAnswers()
     {
         totalCorrectAnswers +=1;
+    }
+
+    public void incrementTotalQuestions()
+    {
+        totalQuestions += 1;
+    }
+
+    public int getOperator()
+    {
+        return operator;
     }
 
 
--- app/src/main/java/com/example/kidsmathpracticeapp/MainActivity.java
+++ app/src/main/java/com/example/kidsmathpracticeapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kidsmathpracticeapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +18,58 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        //create a generator object
+        Generator generator = new Generator();
+
+        initialize(generator);
+
+
+
+
+
+
+
     }
+
+    public void initialize(Generator generator)
+    {
+
+        generator.generate();
+
+        //display number 1
+        TextView output1 = findViewById(R.id.Num1);
+        String strNum1 = generator.getNum1()+"";
+        output1.setText(strNum1);
+
+        //display number 2
+        TextView output2 = findViewById(R.id.Num2);
+        String strNum2 = generator.getNum2()+"";
+        output2.setText(strNum2);
+
+
+
+        //set the operator text view
+        TextView operatorView = findViewById(R.id.Operator);
+        String operator = "";
+        if(generator.getOperator() == 1)
+            operator = "+";
+        if(generator.getOperator() == 2)
+            operator = "-";
+        if(generator.getOperator() == 3)
+            operator = "*";
+        if(generator.getOperator() == 4)
+            operator = "/";
+        operatorView.setText(operator);
+
+
+        //get the total answers and correct answers
+        TextView scoreOutPut = findViewById(R.id.Scores);
+
+        String strCorrectAnswer = generator.getTotalCorrectAnswers()+"";
+        String strTotalQuestions = generator.getTotalQuestions()+"";
+
+        scoreOutPut.setText(strCorrectAnswer + "/" + strTotalQuestions);
+    }
+
+
 }
